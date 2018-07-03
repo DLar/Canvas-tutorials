51e2248a2266ee1842f4351ec184b0c265d3477c
DLar <d_larson25@yahoo.com>
2018-07-03 13:40:48 -0500

    Updates

--- boxes2.js
+++ boxes2.js
@@ -4,13 +4,8 @@
 // Modified to give image mapping percentages by David Larson
 // https://github.com/DLar/Canvas-tutorials/blob/master/boxes2.js
 
-// This is a self-executing function that I added only to stop this
-// new script from interfering with the old one. It's a good idea in general, but not
-// something I wanted to go over during this tutorial
-(function(window) {
-
 // holds all our boxes
-var boxes2 = [];
+var boxes = [];
 
 // New, holds the 8 tiny boxes that will be our selection handles
 // the selection handles will be in this order:
@@ -19,14 +14,14 @@ var boxes2 = [];
 // 5  6  7
 var selectionHandles = [];
 
-// Array for box and table colors (RGB values only)
+// Array for box and table colors
 var colors = [
-'61,113,208',
-'149,41,125',
-'242,10,21',
-'247,173,15',
-'248,250,85',
-'46,208,65'
+'rgba(61,113,208,0.5)',
+'rgba(149,41,125,0.5)',
+'rgba(242,10,21,0.5)',
+'rgba(247,173,15,0.5)',
+'rgba(248,250,85,0.5)',
+'rgba(46,208,65,0.5)'
 ];
 
 var info;
@@ -72,30 +67,30 @@ var offsetx, offsety;
 var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
 
 // Box object to hold data
-function Box2() {
+function Box() {
   this.x = 0;
   this.y = 0;
   this.w = 1; // default width and height?
   this.h = 1;
-  this.fill = '0,0,0';
+  this.fill = 'black';
 }
 
 // New methods on the Box class
-Box2.prototype = {
+Box.prototype = {
   // we used to have a solo draw function
   // but now each box is responsible for its own drawing
   // mainDraw() will call this with the normal canvas
   // myDown will call this with the ghost canvas with 'black'
-  draw: function(context, optionalColor) {
+  draw: function(context) {
       if (context === gctx) {
         context.fillStyle = 'black'; // always want black for the ghost canvas
       } else {
-        context.fillStyle = 'rgba(' + this.fill + ',0.5)';
+        context.fillStyle = this.fill;
       }
       
       // We can skip the drawing of elements that have moved off the screen:
-      if (this.x > WIDTH || this.y > HEIGHT) return;
-      if (this.x + this.w < 0 || this.y + this.h < 0) return;
+      if (this.x > WIDTH || this.y > HEIGHT) { return; }
+      if (this.x + this.w < 0 || this.y + this.h < 0) { return; }
       
       context.fillRect(this.x,this.y,this.w,this.h);
       
@@ -105,7 +100,7 @@ Box2.prototype = {
       context.strokeStyle = 'black';
       context.lineWidth = mySelWidth;
       
-      if (mySelWidth % 2 == 0) {
+      if (mySelWidth % 2 === 0) {
         context.strokeRect(this.x,this.y,this.w,this.h);
       }
       else {
@@ -157,7 +152,7 @@ Box2.prototype = {
     
   } // end draw
 
-}
+};
 
 //Initialize a new Box, add it, and invalidate the canvas
 function addRect(x, y, w, h, fill) {
@@ -165,13 +160,13 @@ function addRect(x, y, w, h, fill) {
     addTable(fill); // Add a new table for each box created
   }
   
-  var rect = new Box2;
+  var rect = new Box();
   rect.x = x;
   rect.y = y;
   rect.w = w;
   rect.h = h;
   rect.fill = fill;
-  boxes2.push(rect);
+  boxes.push(rect);
   mySel = rect;
   preDel++;
   canvasValid = false;
@@ -179,9 +174,7 @@ function addRect(x, y, w, h, fill) {
 
 // initialize our canvas, add a ghost canvas, set draw loop
 // then add everything we want to intially exist on the canvas
-function init2(img) {
-  IMG = img;
-  canvas = document.getElementById('canvas2');
+function init() {
   if (IMG.width > MAXWIDTH) {
     WIDTH = MAXWIDTH;
     HEIGHT = Math.round(IMG.height * MAXWIDTH / IMG.width);
@@ -202,15 +195,15 @@ function init2(img) {
   gctx = ghostcanvas.getContext('2d');
   
   //fixes a problem where double clicking causes text to get selected on the canvas
-  canvas.onselectstart = function () { return false; }
+  canvas.onselectstart = function () { return false; };
   
   // fixes mouse co-ordinate problems when there's a border or padding
   // see getMouse for more detail
   if (document.defaultView && document.defaultView.getComputedStyle) {
-    stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)     || 0;
-    stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)      || 0;
-    styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10) || 0;
-    styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)  || 0;
+    stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null).paddingLeft, 10)     || 0;
+    stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null).paddingTop, 10)      || 0;
+    styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null).borderLeftWidth, 10) || 0;
+    styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null).borderTopWidth, 10)  || 0;
   }
   
   // make mainDraw() fire every INTERVAL milliseconds
@@ -229,7 +222,7 @@ function init2(img) {
   
   // set up the selection handle boxes
   for (var i = 0; i < 8; i ++) {
-    var rect = new Box2;
+    var rect = new Box();
     selectionHandles.push(rect);
   }
   
@@ -246,16 +239,16 @@ function clear(c) {
 // While draw is called as often as the INTERVAL variable demands,
 // It only ever does something if the canvas gets invalidated by our code
 function mainDraw() {
-  if (canvasValid == false) {
+  if (canvasValid === false) {
     clear(ctx);
     
     // Add stuff you want drawn in the background all the time here
     ctx.drawImage(IMG, 0, 0, WIDTH, HEIGHT);
   
     // draw all boxes
-    var l = boxes2.length;
+    var l = boxes.length;
     for (var i = 0; i < l; i++) {
-      boxes2[i].draw(ctx); // we used to call drawshape, but now each box draws itself
+      boxes[i].draw(ctx); // we used to call drawshape, but now each box draws itself
       
       // Update table information
       if (info) {
@@ -406,7 +399,7 @@ function myMove(e){
 
 // Happens when the mouse is clicked in the canvas
 function myDown(e){
-  document.body.style.webkitUserSelect = 'none';
+  document.onselectstart = function() { return false; };
   getMouse(e);
   
   //we are over a selection box
@@ -416,18 +409,17 @@ function myDown(e){
   }
   
   clear(gctx);
-  var l = boxes2.length;
+  var l = boxes.length;
   for (var i = l-1; i >= 0; i--) {
     // draw shape onto ghost context
-    boxes2[i].draw(gctx, 'black');
+    boxes[i].draw(gctx);
     
     // get image data at the mouse x,y pixel
     var imageData = gctx.getImageData(mx, my, 1, 1);
-    var index = (mx + my * imageData.width) * 4;
     
     // if the mouse pixel exists, select and break
-    if (imageData.data[3] > 0 && e.button == 0) {
-      mySel = boxes2[i];
+    if (imageData.data[3] > 0 && e.button === 0) {
+      mySel = boxes[i];
       offsetx = mx - mySel.x;
       offsety = my - mySel.y;
       mySel.x = mx - offsetx;
@@ -449,7 +441,7 @@ function myDown(e){
 }
 
 function myUp(){
-  document.body.style.webkitUserSelect = 'auto';
+  document.onselectstart = function() { return true; };
   
   isDrag = false;
   isResizeDrag = false;
@@ -466,10 +458,15 @@ function myDblClick(e) {
   getMouse(e);
   // for this method width and height determine the starting X and Y, too.
   // so I left them as vars in case someone wanted to make them args for something and copy this code
-  var dwidth = parseInt(document.getElementById("dwidth").value);
-  var dheight = parseInt(document.getElementById("dheight").value);
-  var width = (dwidth) ? dwidth : 200;
-  var height = (dheight) ? dheight : 30;
+  var width = 24;
+  var height = 24;
+  
+  if (width > WIDTH) {
+    width = WIDTH;
+  }
+  if (height > HEIGHT) {
+    height = HEIGHT;
+  }
   
   var x;
   var y;
@@ -501,46 +498,46 @@ function myKeyDown(e) {
   e.preventDefault();
   
   if (mySel !== null) {
-    if (e.shiftKey && e.keyCode == 37) {  /* Shift+Left arrow was pressed */
-      ((mySel.x - 1) < 0) ? mySel.x = 0 : mySel.x--;
+    if (e.shiftKey && e.keyCode === 37) {  /* Shift+Left arrow was pressed */
+      mySel.x = ((mySel.x - 5) < 0) ? 0 : mySel.x - 5;
     }
-    else if (e.shiftKey && e.keyCode == 38) {  /* Shift+Up arrow was pressed */
-      ((mySel.y - 1) < 0) ? mySel.y = 0 : mySel.y--;
+    else if (e.shiftKey && e.keyCode === 38) {  /* Shift+Up arrow was pressed */
+      mySel.y = ((mySel.y - 5) < 0) ? 0 : mySel.y - 5;
     }
-    else if (e.shiftKey && e.keyCode == 39) {  /* Shift+Right arrow was pressed */
-      ((mySel.x + mySel.w + 1) > WIDTH) ? mySel.x = (WIDTH - mySel.w) : mySel.x++;
+    else if (e.shiftKey && e.keyCode === 39) {  /* Shift+Right arrow was pressed */
+      mySel.x = ((mySel.x + mySel.w + 5) > WIDTH) ? WIDTH - mySel.w : mySel.x + 5;
     }
-    else if (e.shiftKey && e.keyCode == 40) {  /* Shift+Down arrow was pressed */
-      ((mySel.y + mySel.h + 1) > HEIGHT) ? mySel.y = (HEIGHT - mySel.h) : mySel.y++;
+    else if (e.shiftKey && e.keyCode === 40) {  /* Shift+Down arrow was pressed */
+      mySel.y = ((mySel.y + mySel.h + 5) > HEIGHT) ? HEIGHT - mySel.h : mySel.y + 5;
     }
-    else if (e.keyCode == 37) {  /* Left arrow was pressed */
-      ((mySel.x - 5) < 0) ? mySel.x = 0 : mySel.x -= 5;
+    else if (e.keyCode === 37) {  /* Left arrow was pressed */
+      mySel.x = ((mySel.x - 1) < 0) ? 0 : mySel.x - 1;
     }
-    else if (e.keyCode == 38) {  /* Up arrow was pressed */
-      ((mySel.y - 5) < 0) ? mySel.y = 0 : mySel.y -= 5;
+    else if (e.keyCode === 38) {  /* Up arrow was pressed */
+      mySel.y = ((mySel.y - 1) < 0) ? 0 : mySel.y - 1;
     }
-    else if (e.keyCode == 39) {  /* Right arrow was pressed */
-      ((mySel.x + mySel.w + 5) > WIDTH) ? mySel.x = (WIDTH - mySel.w) : mySel.x += 5;
+    else if (e.keyCode === 39) {  /* Right arrow was pressed */
+      mySel.x = ((mySel.x + mySel.w + 1) > WIDTH) ? WIDTH - mySel.w : mySel.x + 1;
     }
-    else if (e.keyCode == 40) {  /* Down arrow was pressed */
-      ((mySel.y + mySel.h + 5) > HEIGHT) ? mySel.y = (HEIGHT - mySel.h) : mySel.y += 5;
+    else if (e.keyCode === 40) {  /* Down arrow was pressed */
+      mySel.y = ((mySel.y + mySel.h + 1) > HEIGHT) ? HEIGHT - mySel.h : mySel.y + 1;
     }
-    else if (e.keyCode == 68) {  /* D key was pressed */
+    else if (e.keyCode === 68) {  /* D key was pressed */
       var x = (mySel.x + mySel.w + 5 > WIDTH) ? WIDTH - mySel.w : mySel.x + 5;
       var y = (mySel.y + mySel.h + 5 > HEIGHT) ? HEIGHT - mySel.h : mySel.y + 5;
       addRect(x, y, mySel.w, mySel.h, colors[preDel % colors.length]);
     }
-    else if (e.keyCode == 8 || e.keyCode == 46) {  /* Backspace or Delete was pressed */
-      var l = boxes2.length;
+    else if (e.keyCode === 8 || e.keyCode === 46) {  /* Backspace or Delete was pressed */
+      var l = boxes.length;
       for (var i = 0; i < l; i++) {
-        if (mySel === boxes2[i]) {
-          boxes2.splice(i, 1);
+        if (mySel === boxes[i]) {
+          boxes.splice(i, 1);
           l--;
           info.removeChild( info.lastChild );
           canvasValid = false;
         }
-        if (i != l) {
-          info.childNodes[i].setAttribute( 'style', 'background-color: rgba(' + boxes2[i].fill + ',0.5); border-color: rgb(' + boxes2[i].fill + ');' );
+        if (i !== l) {
+          info.childNodes[i].setAttribute( 'style', 'background-color: ' + boxes[i].fill );
         }
       }
     }
@@ -552,7 +549,7 @@ function myKeyDown(e) {
   }
 }
 
-function myBlur(e) {
+function myBlur() {
   document.body.style.cursor='auto';
   // havent returned means we have selected nothing
   mySel = null;
@@ -617,10 +614,10 @@ function getMouse(e) {
   var element = canvas, offsetX = 0, offsetY = 0;
 
   if (element.offsetParent) {
-	do {
-	  offsetX += element.offsetLeft;
-	  offsetY += element.offsetTop;
-	} while ((element = element.offsetParent));
+    do {
+      offsetX += element.offsetLeft;
+      offsetY += element.offsetTop;
+    } while ((element = element.offsetParent));
   }
 
   // Add padding and border style widths to offset
@@ -635,63 +632,64 @@ function getMouse(e) {
 }
 
 function addTable(color) {
-  var l = boxes2.length; //Length of boxes before new one is added (l-1)
+  var l = boxes.length; //Length of boxes before new one is added (l-1)
   
-  var table = document.createElement('table');
-  table.setAttribute( 'style', 'background-color: rgba(' + color + ',0.5); border-color: rgb(' + color + ');' );
-  table.innerHTML = '  <tr>\
-    <th colspan="2">Link ' + (l+1) + '</th>\
-  </tr>\
-  <tr>\
-    <td>Top:</td>\
-    <td id="top' + l + '"></td>\
-  </tr>\
-  <tr>\
-    <td>Right:</td>\
-    <td id="right' + l + '"></td>\
-  </tr>\
-  <tr>\
-    <td>Bottom:</td>\
-    <td id="bottom' + l + '"></td>\
-  </tr>\
-  <tr>\
-    <td>Left:</td>\
-    <td id="left' + l + '"></td>\
-  </tr>';
-  info.appendChild(table);
+  var row = document.createElement('tr');
+  row.setAttribute( 'style', 'background-color: ' + color );
+  row.innerHTML = '<th>Link ' + (l+1) + '</th>' +
+    '<td>top: <span id="top' + l + '"></span>; right: <span id="right' + l + '"></span>; bottom: <span id="bottom' + l + '"></span>; left: <span id="left' + l + '"></span>;</td>';
+  info.appendChild(row);
 }
 
 function updateTable(index) {
   var cellRef = document.getElementById('top' + index);
-  cellRef.innerHTML = (Math.round(boxes2[index].y / HEIGHT * 1000) / 10) + '%';
+  cellRef.innerHTML = (Math.round(boxes[index].y / HEIGHT * 1000) / 10) + '%';
   cellRef = document.getElementById('right' + index);
-  cellRef.innerHTML = (Math.round((WIDTH - boxes2[index].x - boxes2[index].w) / WIDTH * 1000) / 10) + '%';
+  cellRef.innerHTML = (Math.round((WIDTH - boxes[index].x - boxes[index].w) / WIDTH * 1000) / 10) + '%';
   cellRef = document.getElementById('bottom' + index);
-  cellRef.innerHTML = (Math.round((HEIGHT - boxes2[index].y - boxes2[index].h) / HEIGHT * 1000) / 10) + '%';
+  cellRef.innerHTML = (Math.round((HEIGHT - boxes[index].y - boxes[index].h) / HEIGHT * 1000) / 10) + '%';
   cellRef = document.getElementById('left' + index);
-  cellRef.innerHTML = (Math.round(boxes2[index].x / WIDTH * 1000) / 10) + '%';
+  cellRef.innerHTML = (Math.round(boxes[index].x / WIDTH * 1000) / 10) + '%';
 }
 
-// If you dont want to use <body onLoad='init()'>
-// You could uncomment this init() reference and place the script reference inside the body tag
-//init();
-window.init2 = init2;
-})(window);
-
-function readFile(input) {
-  if (input.files[0]) {
-    var reader = new FileReader();
-    
-    reader.onload = function (e) {
-      var img = new Image;
-      
-      img.onload = function() {
-        init2(img);
-      };
-      
-      img.src = e.target.result;
-    }
-    
-    reader.readAsDataURL(input.files[0]);
+function readFile(file) {
+  if(!file.type.match(/image.*/)) {
+    alert("This file is not an image");
+    return;
   }
+  
+  var reader = new FileReader();
+  
+  reader.onload = function(e) {
+    IMG = new Image();
+    
+    IMG.onload = function() {
+      init();
+    };
+    
+    IMG.src = e.target.result;
+  };
+  
+  reader.readAsDataURL(file);
 }
+
+window.onload = function() {
+  canvas = document.getElementById('canvas2');
+  var file = document.getElementById('file');
+  file.onchange = function() {
+    readFile(file.files[0]);
+  };
+  canvas.ondragover = function(e) {
+    e.preventDefault();
+  };
+  canvas.ondrop = function(e) {
+    e.preventDefault(); 
+    readFile(e.dataTransfer.files[0]);
+  };
+  canvas.onmousedown = function() {
+    return false;
+  };
+  canvas.ondblclick = function() {
+    file.click();
+  };
+};
